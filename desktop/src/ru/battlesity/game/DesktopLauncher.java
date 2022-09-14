79a0d9fa15a7f2650d90c0710ff742b7c26f37b4
Viktor Zakharov <beast666tm@yandex.ru>
2022-09-14 14:20:00 +0300

    Added animation for LibGdx HomeWork 1

--- desktop/src/ru/battlesity/game/DesktopLauncher.java
+++ desktop/src/ru/battlesity/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Battle Sity");
+		//		config.setWindowedMode(1024, 800); // задаёт размер окна
 		new Lwjgl3Application(new TanksGdxGame(), config);
 	}
 }
